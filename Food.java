5ac4c0623939c87d025fe739f1952671b5992fc4
SaravananSivaraj <saravanasiva76390@gmail.com>
2025-10-08 08:13:24 -0700

    data type and variables homework

---
+++ Food.java
@@ -0,0 +1,17 @@
+class Food
+{
+	public static void main (String [] args)
+	{
+		String foodName="Panner Butter Masala";
+		int calories=400;
+		Boolean vegetarian=true;
+		float price=250.50f;
+		String cusineType="Indian";
+		
+		System.out.println(foodName);
+		System.out.println(cusineType);
+		System.out.println(price);
+		System.out.println(vegetarian);
+		
+	}
+}
