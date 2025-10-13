996958d8e2002645fea0029b7c8aa7140859bd52
SaravananSivaraj <saravanasiva76390@gmail.com>
2025-10-13 11:45:24 -0700

    Add files via upload

---
+++ NeonNumber.java
@@ -0,0 +1,13 @@
+class NeonNumber
+{
+	public static void main(String[] args)
+	{
+		int a=9;
+		int b=a*a;
+		int c=a%10;
+		int d=a/10;
+		int e=(d+c);
+		boolean f=e==a;
+		System.out.println(f);
+	}
+}
