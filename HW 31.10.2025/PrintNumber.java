a2e97b9c4d0afd8ad8801f0d510b320c967962dd
SaravananSivaraj <saravanasiva76390@gmail.com>
2025-11-04 23:58:16 +0530

    Add files via upload

---
+++ HW 31.10.2025/PrintNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class PrintNumber
+{
+	public static void main(String [] args)
+	{
+ 		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Array Length : ");
+		int len = sc.nextInt();
+		int ar[] = new int[len];
+		for(int i=0; i<ar.length;i++)
+			{
+			System.out.print("Enter the " + i + " Number: ");
+			ar[i] = sc.nextInt();
+			}
+		for(int i=0; i<len;i++)
+			{
+			System.out.print("The " +i+ " Index value is : " + " " +ar[i]);	
+			}
+			
+	}
+}
